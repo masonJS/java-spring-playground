b1dfa3cc29c2172c532262d1d66f2b71ed4686e7
minwoo <mwoodev526@gmail.com>
2023-07-17 23:55:48 +0900

    init coupon-system

---
+++ coupon-system/src/main/java/com/example/couponsystem/CouponSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.couponsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponSystemApplication.class, args);
+    }
+
+}
---
+++ coupon-system/src/test/java/com/example/couponsystem/CouponSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couponsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
